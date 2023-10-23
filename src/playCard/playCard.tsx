150f5bebdd5284a00ab55f421587c41acf5a521d
Sadyk uulu Azatbek <evgenovicmaksim@gmail.com>
2023-10-23 16:21:25 +0600

    initial commit

--- src/playCard/playCard.tsx
+++ src/playCard/playCard.tsx
@@ -6,10 +6,20 @@ interface Props {
 }
 
 const PlayCard: React.FC<Props> = ({rank, suit}) => {
+    let result: string = '';
+    if (suit === 'diams') {
+        result = '♦';
+    } else if (suit === 'hearts') {
+        result = '♥';
+    } else if (suit === 'clubs') {
+        result = '♣';
+    } else if (suit === 'spades') {
+        result = '♠';
+    }
     return (
         <span className={`card rank-${rank} ${suit}`}>
             <span className="rank">{rank}</span>
-            <span className="suit">{suit}</span>
+            <span className="suit">{result}</span>
         </span>
     );
 };
